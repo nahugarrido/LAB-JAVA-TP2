9d97690223a5182afd364bdb4ede677de72eaa86
Nahuel Garrido <garridonahuel8@gmail.com>
2023-09-11 05:24:34 -0300

    update readme

--- TurnosRotativosTempl/src/main/java/com/neoris/turnosrotativos/config/CorsConfig.java
+++ TurnosRotativosTempl/src/main/java/com/neoris/turnosrotativos/config/CorsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/* Configuracion para poder testear con Stoplight*/
 @Configuration
 public class CorsConfig {
 
